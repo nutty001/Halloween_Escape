ba868f4b110bcea851035964ed5e8bea06624db6
nutty001 <pratikthapa328@gmail.com>
2021-12-16 20:02:14 +0530

    shooting application added

--- Assets/player/scripts/PlayerAttack.cs
+++ Assets/player/scripts/PlayerAttack.cs
@@ -6,9 +6,11 @@ public class PlayerAttack : MonoBehaviour
 {
 
     [SerializeField]private float attackCooldown;
+    [SerializeField] private Transform firePoint;
+    [SerializeField] private GameObject[] fireballs;
     private Animator anim;
     private PlayerMovement playerMovement;
-    private float cooldownTimer;
+    private float cooldownTimer=Mathf.Infinity;
 
     private void Awake()
     {
@@ -25,12 +27,26 @@ public class PlayerAttack : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton(0)&& cooldownTimer>attackCooldown)
+        if (Input.GetMouseButton(0) && cooldownTimer > attackCooldown && playerMovement.canAttack())
             Attack();
+        cooldownTimer += Time.deltaTime;
     }
 
     private void Attack()
     {
+        anim.SetTrigger("attack");
+        cooldownTimer = 0;
 
+        fireballs[FindFireball()].transform.position = firePoint.position;
+        fireballs[FindFireball()].GetComponent<Projectile>().SetDirection(Mathf.Sign(transform.localScale.x));
+    }
+    private int FindFireball()
+    {
+        for (int i = 0; i < fireballs.Length; i++)
+        {
+            if (!fireballs[i].activeInHierarchy)
+                return i;
+        }
+        return 0;
     }
 }
