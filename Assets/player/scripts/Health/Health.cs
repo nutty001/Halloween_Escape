83f8fde6509ab23f9b84d919d3dbfc4b8c874c90
nutty001 <pratikthapa328@gmail.com>
2021-12-18 21:11:10 +0530

    damage

--- Assets/player/scripts/Health/Health.cs
+++ Assets/player/scripts/Health/Health.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class Health : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float startingHealth;
+    public float currentHealth { get; private set; }
+    private Animator anim;
+    private bool dead;
+
+    private void Awake()
     {
+        currentHealth = startingHealth;
+        anim = GetComponent<Animator>();
+    }
+    public void TakeDamage(float _damage)
+    {
+        currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
         
+
+        if(currentHealth>0)
+        {
+            anim.SetTrigger("hurt");
+        }
+        else
+        {
+            if(!dead)
+            {
+                anim.SetTrigger("die");
+                GetComponent<PlayerMovement>().enabled = false;
+                dead = true;
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
---
+++ Assets/player/scripts/Health/HealthBar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField] private Health playerHealth;
+    [SerializeField] private Image totalhealthBar;
+    [SerializeField] private Image currenthealthBar;
+
+    void Start()
+    {
+        totalhealthBar.fillAmount = playerHealth.currentHealth/10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currenthealthBar.fillAmount = playerHealth.currentHealth/10;
+    }
+}
